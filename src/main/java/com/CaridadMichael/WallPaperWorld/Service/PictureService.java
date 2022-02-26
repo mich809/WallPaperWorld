02889bf75687658db23a8a4545dfebed049ff8e0
Michael Caridad <caridadmichael@gmail.com>
2022-02-25 22:15:10 -0500

    Picture upload service

--- src/main/java/com/CaridadMichael/WallPaperWorld/Service/PictureService.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/Service/PictureService.java
@@ -3,14 +3,26 @@ package com.CaridadMichael.WallPaperWorld.Service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.CaridadMichael.WallPaperWorld.model.Picture;
+import com.CaridadMichael.WallPaperWorld.repository.PictureRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class PictureService {
 	
+	private final S3Service s3service;
+	private final PictureRepository pictureRepository;
+	
 	public void uploadPicture(MultipartFile multipartFile) {
-		//upload file to AWS S3
+		String pictureURL = s3service.uploadFile(multipartFile);
 		
+		var picture = new Picture();
+		picture.setUrl(pictureURL);
 		
-		// Save Picture data to database
+		pictureRepository.save(picture);
+		
 	}
 
 }
