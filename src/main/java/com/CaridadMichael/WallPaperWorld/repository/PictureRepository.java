14b96c834a76f30f504713e62435d552afadc897
Michael Caridad <caridadmichael@gmail.com>
2022-02-25 22:17:24 -0500

    created image repo

---
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
@@ -0,0 +1,9 @@
+package com.CaridadMichael.WallPaperWorld.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.CaridadMichael.WallPaperWorld.model.Picture;
+
+public interface PictureRepository extends CrudRepository<Picture, Long> {
+
+}
