817458aa856fbd362df9604353d5dfa823b4b214
Michael Caridad <caridadmichael@gmail.com>
2022-02-25 22:16:08 -0500

    create controller file and endpoint to upload images

--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
@@ -1,5 +1,6 @@
 package com.CaridadMichael.WallPaperWorld.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +9,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.CaridadMichael.WallPaperWorld.Service.PictureService;
+
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/pictures")
+@RequiredArgsConstructor
 public class PictureController {
 	
+	@Autowired
+	private PictureService pictureService;
 	
-	@PostMapping
+	
+	@PostMapping("/addPicture")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void uploadPicture(@RequestParam("file") MultipartFile file) {
+		pictureService.uploadPicture(file);
 		
 	}
 
