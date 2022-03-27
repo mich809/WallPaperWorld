74bd3be98f4f1f7f34c2ba6d022d8c2809569836
Michael Caridad <caridadmichael@gmail.com>
2022-03-27 19:24:38 -0400

    create file for user endpoints

---
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.CaridadMichael.WallPaperWorld.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.CaridadMichael.WallPaperWorld.model.User;
+
+
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
