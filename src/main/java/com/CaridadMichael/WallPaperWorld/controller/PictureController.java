55f3949d59ec8fd9fcc44dfc61879b2404a18214
Michael Caridad <caridadmichael@gmail.com>
2022-03-27 19:07:33 -0400

    added several endpoints
    - Changed requestMapping to "api/picture"
    - added getPictureDetails endpoint
    - added editPicture endpoint
    - modify uploadPicture endpoint , to be able to include name of picture.

--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
@@ -2,7 +2,11 @@ package com.CaridadMichael.WallPaperWorld.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.CaridadMichael.WallPaperWorld.Service.PictureService;
-
+import com.CaridadMichael.WallPaperWorld.dto.PictureDTO;
+import com.CaridadMichael.WallPaperWorld.dto.uploadPictureResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/pictures")
+@RequestMapping("/api/picture")
 @RequiredArgsConstructor
 public class PictureController {
 	
@@ -25,9 +30,22 @@ public class PictureController {
 	
 	@PostMapping("/addPicture")
 	@ResponseStatus(HttpStatus.CREATED)
-	public void uploadPicture(@RequestParam("file") MultipartFile file) {
-		pictureService.uploadPicture(file);
+	public uploadPictureResponse uploadPicture(@RequestParam("fileName") String fileName,@RequestParam("file") MultipartFile file) {
+		return pictureService.uploadPicture(fileName, file);
 		
+	}
+	
+	@PutMapping("/updatePicture")
+	@ResponseStatus(HttpStatus.OK)
+	public PictureDTO editPicture(@RequestBody 	PictureDTO pictureDto) {
+		return pictureService.editPicture(pictureDto);
+		
+	}
+	
+	@GetMapping("/getPictureDetails")
+	@ResponseStatus(HttpStatus.OK)
+	public PictureDTO getPictureDetails(@RequestParam long pictureId) {
+		return pictureService.getPictureDetails(pictureId);
 	}
 
 }
