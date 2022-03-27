2ec88a209c6fe95e967f4ad7b7fe7d7caaed98fe
Michael Caridad <caridadmichael@gmail.com>
2022-03-27 19:04:36 -0400

    added method to get picture by id, also edit picture info and add name to picture

--- src/main/java/com/CaridadMichael/WallPaperWorld/Service/PictureService.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/Service/PictureService.java
@@ -3,6 +3,8 @@ package com.CaridadMichael.WallPaperWorld.Service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.CaridadMichael.WallPaperWorld.dto.PictureDTO;
+import com.CaridadMichael.WallPaperWorld.dto.uploadPictureResponse;
 import com.CaridadMichael.WallPaperWorld.model.Picture;
 import com.CaridadMichael.WallPaperWorld.repository.PictureRepository;
 
@@ -12,16 +14,52 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PictureService {
 	
-	private final S3Service s3service;
+	private final S3StorageService s3Service;
 	private final PictureRepository pictureRepository;
 	
-	public void uploadPicture(MultipartFile multipartFile) {
-		String pictureURL = s3service.uploadFile(multipartFile);
+	public uploadPictureResponse uploadPicture(String fileName,MultipartFile multipartFile) {
+		String pictureURL = s3Service.uploadFile(fileName,multipartFile);
 		
 		var picture = new Picture();
-		picture.setUrl(pictureURL);
+		picture.setPictureUrl(pictureURL);
 		
-		pictureRepository.save(picture);
+		var savedPicture = pictureRepository.save(picture);
+		return new uploadPictureResponse(savedPicture.getId(),savedPicture.getPictureUrl());
+		
+	}
+
+	public PictureDTO editPicture(PictureDTO pictureDto) {
+		// TODO Auto-generated method stub
+		Picture savedPicture = pictureRepository.findById(pictureDto.getId())
+		.orElseThrow(()-> new IllegalArgumentException("Cannot find video by id: "+ pictureDto.getId()));
+		
+		savedPicture.setTags(pictureDto.getTags());
+		savedPicture.setFavorites(pictureDto.getFavorites());
+		savedPicture.setViewCount(pictureDto.getViewCount());
+		pictureRepository.save(savedPicture);
+		
+		return pictureDto;
+		
+		
+	}
+	
+	private Picture getPictureByID(long id) {
+		return pictureRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Cannot find video by id: "+ id));
+	}
+
+	public PictureDTO getPictureDetails(long id) {
+		
+		Picture savedPicture = getPictureByID(id);
+		
+		PictureDTO pictureDTO = new PictureDTO();
+		
+		pictureDTO.setUrl(savedPicture.getPictureUrl());
+		pictureDTO.setTags(savedPicture.getTags());
+		pictureDTO.setFavorites(savedPicture.getFavorites());
+		pictureDTO.setUserId(savedPicture.getUserId());
+		pictureDTO.setViewCount(savedPicture.getViewCount());
+		
+		return pictureDTO;
 		
 	}
 
