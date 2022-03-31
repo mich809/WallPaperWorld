82dda35c85ea17806534be404f06b3b55515fabe
Michael Caridad <caridadmichael@gmail.com>
2022-03-31 15:42:51 -0400

    added @repository annotation

--- src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
@@ -1,9 +1,11 @@
 package com.CaridadMichael.WallPaperWorld.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.WallPaperWorld.model.Picture;
 
+@Repository
 public interface PictureRepository extends CrudRepository<Picture, Long> {
 
 }
--- src/main/java/com/CaridadMichael/WallPaperWorld/repository/UserRepository.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.CaridadMichael.WallPaperWorld.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.WallPaperWorld.model.AppUser;
 
 
-
+@Repository
 public interface UserRepository extends CrudRepository<AppUser, Long>{
 	
 	AppUser findByUsername(String username);
