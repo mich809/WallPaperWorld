e3e618a5bc6744b6f93d806f31a359641e5b6545
Michael Caridad <caridadmichael@gmail.com>
2022-03-31 15:41:39 -0400

    added authenticate enpoint

--- src/main/java/com/CaridadMichael/WallPaperWorld/WallPaperWorldApplication.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/WallPaperWorldApplication.java
@@ -3,11 +3,14 @@ package com.CaridadMichael.WallPaperWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WallPaperWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WallPaperWorldApplication.class, args);
 	}
+	
+	
 
 }
--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.CaridadMichael.WallPaperWorld.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CaridadMichael.WallPaperWorld.utils.AuthenticationRequest;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
 
+	@GetMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest ) throws Exception{
+		return null;
+	}
+	
 }
