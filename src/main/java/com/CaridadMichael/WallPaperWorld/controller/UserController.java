8debee8829b4abcba995be18fa051a75d84d9c54
Michael Caridad <caridadmichael@gmail.com>
2022-04-05 22:17:38 -0400

    Add two new endpoints
    -Add registration endpoint
    -Add Authenticate endpoint
    -remove a few imports

--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
@@ -1,25 +1,41 @@
 package com.CaridadMichael.WallPaperWorld.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.CaridadMichael.WallPaperWorld.utils.AuthenticationRequest;
+import com.CaridadMichael.WallPaperWorld.Service.UserService;
+import com.CaridadMichael.WallPaperWorld.dto.UserDTO;
+
+import com.CaridadMichael.WallPaperWorld.utils.AuthenticationResponse;
 
 import lombok.RequiredArgsConstructor;
+
 
 
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-public class UserController {
 
-	@GetMapping("/authenticate")
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest ) throws Exception{
-		return null;
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<String> saveUser(@RequestBody UserDTO user) {
+	
+		return userService.RegisterUser(user);
+		
 	}
+	
+	@PostMapping({"/authenticate"})
+	public AuthenticationResponse authenticate(@RequestBody UserDTO user) throws Exception {
+	       return userService.createJwtToken(user);
+	   }
 	
 }
