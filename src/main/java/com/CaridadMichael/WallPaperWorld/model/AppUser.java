409620a527934e8a60505d3a38a24cf2f4f9083e
Michael Caridad <caridadmichael@gmail.com>
2022-04-05 22:21:36 -0400

    add new property

--- src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
@@ -1,14 +1,18 @@
 package com.CaridadMichael.WallPaperWorld.model;
 
-import java.util.List;
+
 import java.util.Set;
 
-import javax.persistence.ElementCollection;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,9 +26,13 @@ public class AppUser {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;	
+	
+	@Column(unique=true)
 	private String username;
 	private String password;
-	@ElementCollection(targetClass=String.class)
-	private Set<String> likedPictures;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "AppUser_fk")
+	private Set<Picture> favoritePictures;
  
 }
