58a3182b4299503f4cd2caaa620e8d97312eeb54
Michael Caridad <caridadmichael@gmail.com>
2022-04-05 22:16:58 -0400

    Add @EnableEncryptableaProperties annotation

--- src/main/java/com/CaridadMichael/WallPaperWorld/Service/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.CaridadMichael.WallPaperWorld.Service;
-
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-	String uploadFile(MultipartFile file);
-
-}
--- src/main/java/com/CaridadMichael/WallPaperWorld/WallPaperWorldApplication.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/WallPaperWorldApplication.java
@@ -3,8 +3,11 @@ package com.CaridadMichael.WallPaperWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class WallPaperWorldApplication {
 
 	public static void main(String[] args) {
