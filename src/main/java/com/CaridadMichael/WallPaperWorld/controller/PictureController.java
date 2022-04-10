8fcfaba868109e1716d7722a06bbf307d806b344
Michael Caridad <caridadmichael@gmail.com>
2022-04-09 20:05:34 -0400

    Add new endpoints
    - add getPictureDetails endpoint
    - add getPictures endpoint
    - add increaseViewCount endpoint
    - add getPicturesByTag endpoint
    - modify uploadPicture endpoint , to take picture's name

--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/PictureController.java
@@ -1,21 +1,27 @@
 package com.CaridadMichael.WallPaperWorld.controller;
 
+
+
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.CaridadMichael.WallPaperWorld.Service.PictureService;
 import com.CaridadMichael.WallPaperWorld.dto.PictureDTO;
-import com.CaridadMichael.WallPaperWorld.dto.uploadPictureResponse;
+
+import com.CaridadMichael.WallPaperWorld.model.Picture;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,22 +36,40 @@ public class PictureController {
 	
 	@PostMapping("/addPicture")
 	@ResponseStatus(HttpStatus.CREATED)
-	public uploadPictureResponse uploadPicture(@RequestParam("fileName") String fileName,@RequestParam("file") MultipartFile file) {
-		return pictureService.uploadPicture(fileName, file);
+	public Picture uploadPicture(@RequestParam("picture name") String pictureName,@RequestParam("file") MultipartFile file, @CurrentSecurityContext(expression="authentication.name")String username,@RequestParam("tags") Set<String> pictureTags ) {
+		return pictureService.uploadPicture(pictureName, file,username,pictureTags);
 		
-	}
-	
-	@PutMapping("/updatePicture")
-	@ResponseStatus(HttpStatus.OK)
-	public PictureDTO editPicture(@RequestBody 	PictureDTO pictureDto) {
-		return pictureService.editPicture(pictureDto);
-		
-	}
+	}	
+
 	
 	@GetMapping("/getPictureDetails")
 	@ResponseStatus(HttpStatus.OK)
 	public PictureDTO getPictureDetails(@RequestParam long pictureId) {
 		return pictureService.getPictureDetails(pictureId);
 	}
+	
+	@GetMapping("/getPictures")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody Iterable<Picture> getPictures() {
+	   return pictureService.getAllPictures();
+	}
+	
+	@PutMapping("/increaseViewCount")
+	@ResponseStatus(HttpStatus.OK)
+	public void increaseViewCount (@RequestParam long pictureId) {
+	   pictureService.increaseViewCount(pictureId);
+	}
+	
+	@GetMapping("/getPicturesByTag")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody Iterable<Picture> getPicturesByTag(@RequestParam Set<String> tags) {
+	   return pictureService.searchByTag(tags);
+	}
+	
+	
+	
+	
+	
+
 
 }
