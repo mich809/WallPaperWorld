cc057e5b639288da21f37918fbd8e3b782fd20af
Michael Caridad <caridadmichael@gmail.com>
2022-04-11 20:49:44 -0400

    Add several endpoints
    - saveUser -> register new user to the database
    - authenticate -> authenticate user
    - addToFavorites -> add a pictuer to a user's favorites
    - removeFromFavorites -> remove a picture from a user's favorites
    - getUser ->  get user from the database
    - getFavorites -> get all of a user's favorite pictures.

--- src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.CaridadMichael.WallPaperWorld.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.CaridadMichael.WallPaperWorld.Service.UserService;
 import com.CaridadMichael.WallPaperWorld.dto.UserDTO;
-
+import com.CaridadMichael.WallPaperWorld.model.AppUser;
+import com.CaridadMichael.WallPaperWorld.model.Picture;
 import com.CaridadMichael.WallPaperWorld.utils.AuthenticationResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -20,22 +27,47 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<String> saveUser(@RequestBody UserDTO user) {
-	
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<String> saveUser(@RequestBody UserDTO user) {	
 		return userService.RegisterUser(user);
 		
 	}
 	
-	@PostMapping({"/authenticate"})
+	@PostMapping("/authenticate")
 	public AuthenticationResponse authenticate(@RequestBody UserDTO user) throws Exception {
 	       return userService.createJwtToken(user);
-	   }
+	}
+	
+	@PutMapping("/AddToFavorites")
+	public void addToFavorites(@RequestParam String username , @RequestParam long pictureID) {
+		userService.addToFavorites(username,pictureID);
+		
+	}
+	
+	@PutMapping("/removeFromFavorites")
+	public void removeFromFavorites(@RequestParam String username , @RequestParam long pictureID) {
+		userService.removeFromFavorites(username, pictureID);
+		
+	}
+	
+	@GetMapping("/getUser")
+	public AppUser getUser(@RequestParam String username) {
+		return userService.getUserByUsername(username);
+		
+	}
+	
+	@GetMapping("/getFavorites")
+	public @ResponseBody Iterable<Picture> getFavorites(@RequestParam String username) {
+		return userService.getFavorites(username);
+		
+	}
+	
+	
 	
 }
