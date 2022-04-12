6b9d18d227dd93c689e5e630bb3809486473828b
Michael Caridad <caridadmichael@gmail.com>
2022-04-11 21:56:23 -0400

    added 3 methods : increaseViewCount, increaseFavoriteCount and decrease FavoriteCount

--- src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
@@ -1,11 +1,38 @@
 package com.CaridadMichael.WallPaperWorld.repository;
 
+
+
+
+
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.WallPaperWorld.model.Picture;
 
 @Repository
 public interface PictureRepository extends CrudRepository<Picture, Long> {
+	
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE Picture  set view_count = view_count + 1 WHERE id = :id")
+	void increaseViewCount(@Param("id") Long id );
+	
+	@Modifying
+	@Query(nativeQuery = true, value ="UPDATE Picture  set favorites = favorites + 1 WHERE id = :id")
+	void increaseFavoriteCount(@Param("id") Long id );
+	
+	@Modifying
+	@Query(nativeQuery = true, value ="UPDATE Picture  set favorites = favorites - 1 WHERE id = :id")	
+	void decreaseFavoriteCount(@Param("id") Long id );
+	
+	
+	List<Picture> findByTagsIn(Set<String> tags);
+
 
 }
