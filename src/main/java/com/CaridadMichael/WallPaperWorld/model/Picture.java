af64a7b805b356a68ee3576d101809a650480e4d
Michael Caridad <caridadmichael@gmail.com>
2022-04-11 21:55:35 -0400

    added a manyToOne user

--- src/main/java/com/CaridadMichael/WallPaperWorld/model/Picture.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/model/Picture.java
@@ -2,13 +2,15 @@ package com.CaridadMichael.WallPaperWorld.model;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,14 +23,24 @@ import lombok.NoArgsConstructor;
 public class Picture {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;	
-	private String userId;
+	private long id;		
+	
+	private String pictureName;
+	private String uploadedBy;	
+	
 	private Integer favorites;
 	private Integer viewCount;	
+	
 	@ElementCollection(targetClass=String.class)
 	private Set<String> tags;
-	private String pictureUrl;
 	
+	private String pictureUrl;	
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "username", referencedColumnName = "username")
+	private AppUser user;
+	
+
 	
 
 }
