25d312152c5fcb92aa26bb9ea007d651e84adf9d
Michael Caridad <caridadmichael@gmail.com>
2022-04-11 21:55:01 -0400

    added oneToMany pictures

--- src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
@@ -5,13 +5,18 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,12 +32,31 @@ public class AppUser {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;	
 	
-	@Column(unique=true)
+	@Column(unique=true)	
 	private String username;
+	
 	private String password;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "AppUser_fk")
+	@ElementCollection(targetClass=Picture.class)
 	private Set<Picture> favoritePictures;
+	
+	@JsonIgnore
+	  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL)
+	private Set<Picture> pictures;
+	
+	
+	
+	
+	
+	 public void addPicture(Picture picture){
+	        favoritePictures.add(picture);
+	 }
+	 
+	 public void removePicture(Picture picture){
+	        favoritePictures.remove(picture);
+	 }
+	
+
  
 }
