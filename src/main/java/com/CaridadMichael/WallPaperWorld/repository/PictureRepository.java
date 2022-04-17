7db2ed6404f81955c3b2d0f8435c5257472e71ea
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 22:17:01 -0400

    added some native queries

--- src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/repository/PictureRepository.java
@@ -1,10 +1,5 @@
 package com.CaridadMichael.WallPaperWorld.repository;
 
-
-
-
-
-
 import java.util.List;
 import java.util.Set;
 
@@ -18,21 +13,22 @@ import com.CaridadMichael.WallPaperWorld.model.Picture;
 
 @Repository
 public interface PictureRepository extends CrudRepository<Picture, Long> {
-	
+
 	@Modifying
 	@Query(nativeQuery = true, value = "UPDATE Picture  set view_count = view_count + 1 WHERE id = :id")
-	void increaseViewCount(@Param("id") Long id );
-	
+	void increaseViewCount(@Param("id") Long id);
+
 	@Modifying
-	@Query(nativeQuery = true, value ="UPDATE Picture  set favorites = favorites + 1 WHERE id = :id")
-	void increaseFavoriteCount(@Param("id") Long id );
-	
+	@Query(nativeQuery = true, value = "UPDATE Picture  set favorites = favorites + 1 WHERE id = :id")
+	void increaseFavoriteCount(@Param("id") Long id);
+
 	@Modifying
-	@Query(nativeQuery = true, value ="UPDATE Picture  set favorites = favorites - 1 WHERE id = :id")	
-	void decreaseFavoriteCount(@Param("id") Long id );
-	
-	
+	@Query(nativeQuery = true, value = "UPDATE Picture  set favorites = favorites - 1 WHERE id = :id")
+	void decreaseFavoriteCount(@Param("id") Long id);
+
 	List<Picture> findByTagsIn(Set<String> tags);
 
+	@Query(nativeQuery = true, value = "SELECT * FROM Picture ORDER BY random() LIMIT 3")
+	List<Picture> getRandomPictures();
 
 }
