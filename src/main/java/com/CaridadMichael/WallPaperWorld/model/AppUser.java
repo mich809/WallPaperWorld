566afe5f0a40a2687053cef52a012118201c28ef
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 21:59:36 -0400

    remove data annotation due to some conflicts

--- src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/model/AppUser.java
@@ -1,6 +1,6 @@
 package com.CaridadMichael.WallPaperWorld.model;
 
-
+import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -10,53 +10,51 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class AppUser {
-	
+@Setter
+@Getter
+public class AppUser implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;	
-	
-	@Column(unique=true)	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(unique = true)
 	private String username;
-	
+
 	private String password;
 
-	@ElementCollection(targetClass=Picture.class)
+	@ElementCollection(targetClass = Picture.class)
 	private Set<Picture> favoritePictures;
-	
-	@JsonIgnore
-	  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,
-      cascade = CascadeType.ALL)
-	private Set<Picture> pictures;
-	
-	
-	
-	
-	
-	 public void addPicture(Picture picture){
-	        favoritePictures.add(picture);
-	 }
-	 
-	 public void removePicture(Picture picture){
-	        favoritePictures.remove(picture);
-	 }
-	
 
- 
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Set<Picture> pictures;
+
+	public void addPicture(Picture picture) {
+		favoritePictures.add(picture);
+	}
+
+	public void removePicture(Picture picture) {
+		favoritePictures.remove(picture);
+	}
+
+	public void uploadedPictures(Picture picture) {
+		pictures.add(picture);
+	}
+
 }
