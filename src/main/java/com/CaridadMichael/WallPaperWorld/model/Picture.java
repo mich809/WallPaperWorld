b0d9a3cffcdb7cc06a6722d819435c975ceb40d8
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 22:00:43 -0400

    nothing major

--- src/main/java/com/CaridadMichael/WallPaperWorld/model/Picture.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/model/Picture.java
@@ -5,42 +5,43 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Data
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Setter
+@Getter
 public class Picture {
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;		
-	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
 	private String pictureName;
-	private String uploadedBy;	
-	
+	private String uploadedBy;
+
 	private Integer favorites;
-	private Integer viewCount;	
-	
-	@ElementCollection(targetClass=String.class)
+	private Integer viewCount;
+	private String pictureUrl;
+
+	@ElementCollection(targetClass = String.class)
 	private Set<String> tags;
-	
-	private String pictureUrl;	
-	
+
+	@JsonIgnore
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "username", referencedColumnName = "username")
 	private AppUser user;
-	
-
-	
 
 }
