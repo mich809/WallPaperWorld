575c5007204cbaace8386b2b714865c84ddb58c2
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 21:56:21 -0400

    modify loadByUsername method

--- src/main/java/com/CaridadMichael/WallPaperWorld/config/CustomUserDetailsService.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/config/CustomUserDetailsService.java
@@ -1,9 +1,11 @@
 package com.CaridadMichael.WallPaperWorld.config;
 
-import java.util.ArrayList;
+
+import java.util.Collections;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,9 +15,9 @@ import org.springframework.stereotype.Service;
 import com.CaridadMichael.WallPaperWorld.model.AppUser;
 import com.CaridadMichael.WallPaperWorld.repository.UserRepository;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 	
@@ -26,9 +28,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {		
 		Optional<AppUser> user = userRepo.findByUsername(username);
-		AppUser u = user.orElseThrow(()-> new UsernameNotFoundException("custom user detail : 	User not found in database"));
-		log.info("User {} found in database",username);		
-		return new User(u.getUsername(),u.getPassword(), new ArrayList<>());
+		AppUser u = user.orElseThrow(()-> new UsernameNotFoundException("Could not find user with username: " + username));
+			
+		return new User(u.getUsername(),u.getPassword(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
 	}
 
 }
