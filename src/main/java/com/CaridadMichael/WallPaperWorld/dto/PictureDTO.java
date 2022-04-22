1ff0e0d624fee23d6299ae6247816fe46896832e
Michael Caridad <caridadmichael@gmail.com>
2022-04-21 21:00:39 -0400

    remove pictureName property

--- src/main/java/com/CaridadMichael/WallPaperWorld/dto/PictureDTO.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/dto/PictureDTO.java
@@ -12,12 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PictureDTO {
-	private String pictureName;
 	private String url;
 	private String uploadedBy;
 	private Integer favorites;
 	private Integer viewCount;
-	@ElementCollection(targetClass=String.class)
+	@ElementCollection(targetClass = String.class)
 	private Set<String> tags;
 
 }
