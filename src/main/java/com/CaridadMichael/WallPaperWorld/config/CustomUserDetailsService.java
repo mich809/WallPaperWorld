6c7929f5102dfce5b56712ce1c031d2e0843531f
Michael Caridad <caridadmichael@gmail.com>
2022-05-08 21:41:36 -0400

    autowired role repo and added authorities to users

--- src/main/java/com/CaridadMichael/WallPaperWorld/config/CustomUserDetailsService.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/config/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.CaridadMichael.WallPaperWorld.config;
 
-
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,22 +15,27 @@ import org.springframework.stereotype.Service;
 import com.CaridadMichael.WallPaperWorld.model.AppUser;
 import com.CaridadMichael.WallPaperWorld.repository.UserRepository;
 
-
-
-
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-	
 
-    @Autowired
-	private  UserRepository userRepo;
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {		
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<AppUser> user = userRepo.findByUsername(username);
-		AppUser u = user.orElseThrow(()-> new UsernameNotFoundException("Could not find user with username: " + username));
-			
-		return new User(u.getUsername(),u.getPassword(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+		AppUser u = user
+				.orElseThrow(() -> new UsernameNotFoundException("Could not find user with username: " + username));
+
+		return new User(u.getUsername(), u.getPassword(), getAuthority(u));
+	}
+
+	private Set<SimpleGrantedAuthority> getAuthority(AppUser user) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		user.getRole().forEach(role -> {
+			authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+		});
+		return authorities;
 	}
 
 }
