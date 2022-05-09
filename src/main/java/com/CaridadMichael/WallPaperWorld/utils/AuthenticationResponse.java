51f6985850e50eb88eae22456a5fd1fd9262ba3e
Michael Caridad <caridadmichael@gmail.com>
2022-05-08 22:46:18 -0400

    add role variable

--- src/main/java/com/CaridadMichael/WallPaperWorld/utils/AuthenticationResponse.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/utils/AuthenticationResponse.java
@@ -1,15 +1,17 @@
 package com.CaridadMichael.WallPaperWorld.utils;
 
+import java.util.Set;
+
+import com.CaridadMichael.WallPaperWorld.model.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 
 @Data
 @AllArgsConstructor
 public class AuthenticationResponse {
 	private final String username;
 	private final String jwt;
-
-
+	private Set<Role> role;
 
 }
