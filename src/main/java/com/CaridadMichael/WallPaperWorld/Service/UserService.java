b6068b87e75533e2c45d22c221d9d2026005b2b7
Michael Caridad <caridadmichael@gmail.com>
2022-05-08 21:41:09 -0400

    autowire role repository

--- src/main/java/com/CaridadMichael/WallPaperWorld/Service/UserService.java
+++ src/main/java/com/CaridadMichael/WallPaperWorld/Service/UserService.java
@@ -1,5 +1,8 @@
 package com.CaridadMichael.WallPaperWorld.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +18,20 @@ import com.CaridadMichael.WallPaperWorld.config.CustomUserDetailsService;
 import com.CaridadMichael.WallPaperWorld.dto.UserDTO;
 import com.CaridadMichael.WallPaperWorld.model.AppUser;
 import com.CaridadMichael.WallPaperWorld.model.Picture;
+import com.CaridadMichael.WallPaperWorld.model.Role;
 import com.CaridadMichael.WallPaperWorld.repository.PictureRepository;
+import com.CaridadMichael.WallPaperWorld.repository.RoleRepo;
 import com.CaridadMichael.WallPaperWorld.repository.UserRepository;
 import com.CaridadMichael.WallPaperWorld.utils.AuthenticationResponse;
 import com.CaridadMichael.WallPaperWorld.utils.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class UserService {
 
 	@Autowired
@@ -35,6 +42,9 @@ public class UserService {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private RoleRepo roleRepo;
 
 	@Autowired
 	private JwtUtil jwtUtil;
@@ -50,10 +60,13 @@ public class UserService {
 			return new ResponseEntity<String>("User " + user.getUsername() + " already Exist", HttpStatus.CONFLICT);
 
 		}
-
+		Role role = roleRepo.findById("User").get();
+		Set<Role> userRoles = new HashSet<>();
+		userRoles.add(role);
 		AppUser newUser = new AppUser();
 		newUser.setUsername(user.getUsername());
 		newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+		newUser.setRole(userRoles);
 		userRepo.save(newUser);
 		return new ResponseEntity<String>("User " + newUser.getUsername() + " registered", HttpStatus.CREATED);
 	}
@@ -66,8 +79,9 @@ public class UserService {
 
 			UserDetails userDetails = customUserDetailsService.loadUserByUsername(user.getUsername());
 			String token = jwtUtil.generateToken(userDetails);
+			AppUser u = getUserByUsername(user.getUsername());
 
-			return new AuthenticationResponse(user.getUsername(), token);
+			return new AuthenticationResponse(user.getUsername(), token, u.getRole());
 		} catch (AuthenticationException authExc) {
 			throw new RuntimeException("Invalid Login Credentials");
 		}
@@ -75,25 +89,24 @@ public class UserService {
 	}
 
 	public void addToFavorites(String username, String pictureName) {
+		log.info("about to add {} ", pictureName);
 		AppUser appUser = getUserByUsername(username);
 		Picture picture = getPictureByName(pictureName);
 		picture.setFavorites(picture.getFavorites() + 1);
 		appUser.addPicture(picture);
-
 		userRepo.save(appUser);
 		pictureRepo.save(picture);
 
 	}
 
 	public void removeFromFavorites(String username, String pictureName) {
+		log.info("about to remove {} ", pictureName);
 		AppUser appUser = getUserByUsername(username);
 		Picture picture = getPictureByName(pictureName);
 		picture.setFavorites(picture.getFavorites() - 1);
 		appUser.removePicture(picture);
-
 		userRepo.save(appUser);
 		pictureRepo.save(picture);
-
 	}
 
 	public Iterable<Picture> getFavorites(String username) {
@@ -106,18 +119,23 @@ public class UserService {
 				.orElseThrow(() -> new IllegalArgumentException("Cannot find username: " + username));
 	}
 
-	private Picture getPictureByName(String name) {
-		return pictureRepo.getPictureByPictureName(name);
+	private Picture getPictureByName(String pictureName) {
+		log.info("inside getPictureByName method , adding picture {} to favorites ", pictureName);
+		return pictureRepo.getPictureByName(pictureName)
+				.orElseThrow(() -> new IllegalArgumentException("Cannot find username: " + pictureName));
+
 	}
 
 	public UserDTO getUserInfo(String name) {
 		UserDTO user = new UserDTO();
+		AppUser appUser = getUserByUsername(name);
 		user.setPassword("");
 		user.setUsername(name);
-		user.setFavoritePicsCount(getUserByUsername(name).getFavoritePictures().size());
-		user.setUploadedPicsCount(getUserByUsername(name).getPictures().size());
-		user.setFavoritePictures(getUserByUsername(name).getFavoritePictures());
-		user.setPictures(getUserByUsername(name).getPictures());
+		user.setFavoritePicsCount(appUser.getFavoritePictures().size());
+		user.setUploadedPicsCount(appUser.getPictures().size());
+		user.setFavoritePictures(appUser.getFavoritePictures());
+		user.setPictures(appUser.getPictures());
+		user.setDate(appUser.getDate());
 
 		return user;
 	}
